9804c943854828be99393f75b693502031d18aee
Taiyeagboola <taiyeagboola2003@gmail.com>
2022-08-19 01:15:07 +0100

    CRUD App

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 
+from email import message
+from operator import index
+from os import stat
 from turtle import pos
 from typing import Optional
 from fastapi import FastAPI, Response,  status, HTTPException
@@ -28,6 +31,11 @@ def find_post(id):
     for p in my_posts:
         if p['id'] == id:
             return p
+
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p ['id'] ==  id:
+            return i
 
 @app.get("/")
 def read_root():
@@ -63,3 +71,30 @@ def get_post(id: int):
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return {"message": f"post with id {id} was not found"}
     return{"post_detail": post}
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    #deleting post
+    # find the index in the array that has the required ID
+    # my_post.pop(index)
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: ({id}) does not exist")
+
+    my_posts.pop(index)
+    # return{"message": 'post was successfully deleted'}
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return{"data": post_dict}
+    print(post)
+    return{"message": "updated post"}
